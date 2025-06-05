76134df076e94108231123bd3da4a27b03df1438
Phạm Công Liệu <127382362+phamconglieu22052003@users.noreply.github.com>
2025-06-05 20:22:19 +0700

    Update weather_service.dart

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -4,7 +4,7 @@ import 'package:geolocator/geolocator.dart';
 
 class WeatherService {
   final String apiKey =
-      "e893bdcd91254d57939190711250406"; // <--- Thay bằng key riêng nếu cần
+      "e893bdcd91254d57939190711250406";
   final String forecastBaseUrl = 'http://api.weatherapi.com/v1/forecast.json';
   final String searchBaseUrl = 'http://api.weatherapi.com/v1/search.json';
   final String currentWeatherBaseUrl =
